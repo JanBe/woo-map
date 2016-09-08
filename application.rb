d79f8d7ace62dbdedb6c86ba658abc599fb851dc
Jan Bechstein <jan@bechstein.me>
2016-09-09 01:54:09 +0200

    Add action to return woo community sessions

--- application.rb
+++ application.rb
@@ -3,6 +3,7 @@ require 'slim'
 require 'sass'
 require 'coffee-script'
 require 'rack/env'
+require 'httpi'
 
 use Rack::Env unless ENV['RACK_ENV'] == 'production'
 
@@ -16,4 +17,24 @@ end
 
 get '/map.css' do
   sass :map
+end
+
+get '/sessions' do
+  content_type :json
+  request = HTTPI::Request.new(
+    url: "#{ENV['WOO_PRODUCTION_API_URL']}/session/activity",
+    body: {
+      token: token,
+      offset: 0,
+      pageSize: 100,
+      target: 'community'
+    }
+  )
+  HTTPI.post(request).body
+end
+
+def token
+  request = HTTPI::Request.new(url: "#{ENV['WOO_TOKEN_APP_URL']}/tokens/latest")
+  response = HTTPI.get(request)
+  JSON(response.body)['access_token']
 end
