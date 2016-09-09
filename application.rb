e768f9d703de41cc5d1d07fe2fef5e1c606811c4
Jan Bechstein <jan@bechstein.me>
2016-09-09 02:24:55 +0200

    Query spots for sessions and clean up output

--- application.rb
+++ application.rb
@@ -30,11 +30,47 @@ get '/sessions' do
       target: 'community'
     }
   )
-  HTTPI.post(request).body
+
+  JSON(HTTPI.post(request).body)['items'].collect do |session|
+    {
+      max_air_time: session['maxAirTime'],
+      highest_air: session['highestAir'],
+      max_crash_power: session['maxCrashPower'],
+      user_first_name: session['user_name'],
+      user_last_name: session['user_lastname'],
+      created: session['created'],
+      time: session['time'],
+      spot: find_spot(session['_spot']['$id'])
+    }
+  end.to_json
 end
 
 def token
   request = HTTPI::Request.new(url: "#{ENV['WOO_TOKEN_APP_URL']}/tokens/latest")
   response = HTTPI.get(request)
   JSON(response.body)['access_token']
+end
+
+def find_spot(id)
+  spot = spots.find {|s| s['id'] == id }
+  unless spot.nil?
+    {
+      name: spot['properties']['name'],
+      location: {
+        lat: spot['geometry']['coordinates'][0],
+        lon: spot['geometry']['coordinates'][1]
+      }
+    }
+  else
+    {}
+  end
+end
+
+def spots
+  @spots ||= get_spots
+end
+
+def get_spots
+  request = HTTPI::Request.new(url: "#{ENV['WOO_EXPLORE_URL']}/output.geojson")
+  JSON(HTTPI.get(request).body)
 end
