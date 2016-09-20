f5c3f901001549331871059a69fd262069fd22fb
Jan Bechstein <jan@bechstein.me>
2016-09-20 23:02:07 +0200

    Stop requesting new sessions from the WOO server when the returned sessions are older than those that we already know

--- application.rb
+++ application.rb
@@ -39,8 +39,11 @@ end
 
 def load_new_sessions_since(break_time)
   token = get_token
+  break_time_reached = false
 
-  api_sessions = (0..200).step(15).collect do |offset|
+  (0..200).step(15).collect do |offset|
+    break if break_time_reached
+
     request = HTTPI::Request.new(
       url: "#{ENV['WOO_PRODUCTION_API_URL']}/session/activity",
       body: {
@@ -50,34 +53,37 @@ def load_new_sessions_since(break_time)
         target: 'community'
       }
     )
-    JSON(HTTPI.post(request).body)['items']
-  end.flatten!
+    JSON(HTTPI.post(request).body)['items'].each do |api_session|
+      posted_at = Time.at(api_session['created']).to_datetime
 
-  api_sessions.each do |api_session|
-    posted_at = Time.at(api_session['created']).to_datetime
-    break if posted_at < break_time
-    Session.find_or_initialize_by(woo_id: api_session['_id']['$id']) do |session|
-      session.max_airtime = api_session['maxAirTime']
-      session.highest_jump = api_session['highestAir']
-      session.total_height = api_session['totalHeight']
-      session.total_airtime =  api_session['totalAirTime']
-      session.duration = api_session['duration']
-      session.number_of_jumps = api_session['numberOfAirs']
-      session.description = api_session['name']
-      session.max_crash_power = api_session['maxCrashPower']
-      session.user_name = "#{api_session['user_name']} #{api_session['user_lastname']}"
-      session.posted_at = Time.at(api_session['created']).to_datetime
-      session.finished_at = Time.at(api_session['time']).to_datetime
-      session.likes = api_session['totallikes']
-      session.comments = api_session['totalcomments']
-      session.spot_woo_id = api_session['_spot']['$id']
-      api_session['user_pictures'].each do |api_picture|
-        session.pictures.find_or_initialize_by(url: api_picture['url'], picture_type: api_picture['type'])
+      if posted_at < break_time
+        break_time_reached = true
+        break
       end
-      if api_session['_pictures'].any?
-        session.pictures.find_or_initialize_by(url: api_session['_pictures'][0]['url'], picture_type: api_session['_pictures'][0]['type'])
+
+      Session.find_or_initialize_by(woo_id: api_session['_id']['$id']) do |session|
+        session.max_airtime = api_session['maxAirTime']
+        session.highest_jump = api_session['highestAir']
+        session.total_height = api_session['totalHeight']
+        session.total_airtime =  api_session['totalAirTime']
+        session.duration = api_session['duration']
+        session.number_of_jumps = api_session['numberOfAirs']
+        session.description = api_session['name']
+        session.max_crash_power = api_session['maxCrashPower']
+        session.user_name = "#{api_session['user_name']} #{api_session['user_lastname']}"
+        session.posted_at = posted_at
+        session.finished_at = Time.at(api_session['time']).to_datetime
+        session.likes = api_session['totallikes']
+        session.comments = api_session['totalcomments']
+        session.spot_woo_id = api_session['_spot']['$id']
+        api_session['user_pictures'].each do |api_picture|
+          session.pictures.find_or_initialize_by(url: api_picture['url'], picture_type: api_picture['type'])
+        end
+        if api_session['_pictures'].any?
+          session.pictures.find_or_initialize_by(url: api_session['_pictures'][0]['url'], picture_type: api_session['_pictures'][0]['type'])
+        end
+        session.save
       end
-      session.save
     end
   end
 end
