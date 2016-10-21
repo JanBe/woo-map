4a51fcbacd461cecdfdfafd662c65b6491e02851
Jan Bechstein <jan@bechstein.me>
2016-10-21 02:43:39 +0200

    Add an additional heat layer
    For every session on the map a heat spot is added, the intensity depends on the number of jumps in the session

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w[windytv-2.0/*]
+Rails.application.config.assets.precompile += %w[leaflet-heat]
